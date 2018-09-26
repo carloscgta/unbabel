a313d1d08b2b991cc13adccc45a2a7b670ccce23
Carlos Gabriel Teixeira de Almeida <carloscgta@gmail.com>
2018-09-25 23:40:03 -0300

    Initial commit

---
+++ Config/HooksAnnotationTool.cs
@@ -0,0 +1,29 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+namespace Unbabel_AutomationAnnotationTool.Config
+{
+    class HooksAnnotationTool
+    {
+        public static IWebDriver  driver { get; private set; }
+
+        public string screenshotsPasta;
+        int contador = 1;
+
+        public void startTestObject()
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://staging.annotation.tools.unbabel.com/");
+
+        
+        }
+
+        public void AfterScenario()
+        {
+            driver.Quit();
+        }
+
+      
+    }
+}
---
+++ Pages/Base.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using System;
+using Unbabel_AutomationAnnotationTool.Config;
+using OpenQA.Selenium.Support.UI;
+
+namespace Unbabel_AutomationAnnotationTool.Pages
+{
+    class Base
+    {
+
+        WebDriverWait wait;
+        
+        public IWebDriver  driver
+
+        {
+            get
+            {
+                return HooksAnnotationTool.driver;
+            }
+        }
+
+        public void clicar(By element)
+        {
+
+            driver.FindElement(element).Click();
+        }
+
+        public void preencherTexto(By element, string texto)
+        {
+
+            driver.FindElement(element).SendKeys(texto);
+        }
+
+        public static void Wait(int miliseconds, int maxTimeOutSeconds = 60)
+        {
+            var wait = new WebDriverWait(HooksAnnotationTool.driver, new TimeSpan(0, 0, 1, maxTimeOutSeconds));
+            var delay = new TimeSpan(0, 0, 0, 0, miliseconds);
+            var timestamp = DateTime.Now;
+            wait.Until(webDriver => (DateTime.Now - timestamp) > delay);
+        }
+
+
+
+    }
+}
